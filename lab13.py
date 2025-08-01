8d09e357542ad089f5a946814bdca0d513e56a3c
Prabin Bashyal <prabinbashyal0@gmail.com>
2025-08-01 12:25:41 +0545

    ..

--- lab13.py
+++ lab13.py
@@ -18,3 +18,15 @@ for i in range(1, n):
     sum+=y(x[i])
 integral=(h/2)*(y(x[0]) + 2*sum + y(x[n]))
 print(f'The value of integral by trapezodial rule is: {integral}')
+xval=np.linspace(a-10, b+10, 1000)
+plt.plot(xval,[y(x) for x in xval], label='Integrand function')
+plt.plot(x,[y(x) for x in x], label='partition points')
+plt.axhline(0,0)
+plt.axvline(0,0)
+
+ypoints=[y(x) for x in x]
+for i in range(n):
+    x_list=[x[i],x[i],x[i+1],x[i+1]]
+    y_list=[0,ypoints[i],ypoints[i+1],0]
+    plt.fill(x_list, y_list, color='blue',edgecolor='black', alpha=0.5, label='Area under curve')
+plt.show()
